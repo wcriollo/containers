bb8a5bc67deab337123626e445213872f21c6a6d
wcriollo <wilson_criollo@hotmail.com>
2025-05-08 14:14:12 -0500

    Update WeatherForecastController.cs
    ok

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -53,7 +53,7 @@ namespace API_GIT.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-                Environment = "API-CAMBIOS-WCJ :" + key
+                Environment = "API-CAMBIOS-WCJ DSA :" + key
             })
             .ToArray();
         }
